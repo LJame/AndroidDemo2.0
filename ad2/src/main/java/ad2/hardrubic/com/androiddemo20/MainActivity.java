58405c810ab4989b793aa09ca6d57ca95f0b773c
dgzx106 <dgzx106@163.com>
2015-11-16 17:27:11 +0800

    建立工程

---
+++ ad2/src/main/java/ad2/hardrubic/com/androiddemo20/MainActivity.java
@@ -0,0 +1,13 @@
+package ad2.hardrubic.com.androiddemo20;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
