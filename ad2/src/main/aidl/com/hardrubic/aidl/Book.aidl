769eef41a2d2d65666979ba28616a19ed8aa5a91
dgzx106 <dgzx106@163.com>
2015-11-29 18:17:21 +0800

    test aidl

---
+++ ad2/src/main/aidl/com/hardrubic/aidl/Book.aidl
@@ -0,0 +1,6 @@
+package com.hardrubic.aidl;
+
+/**
+ * Book类在AIDL中的声明
+ */
+parcelable Book;
---
+++ ad2/src/main/aidl/com/hardrubic/aidl/Book.java
@@ -0,0 +1,52 @@
+package com.hardrubic.aidl;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Book implements Parcelable{
+    private Long id;
+    private String name;
+
+    protected Book(Parcel in) {
+        name = in.readString();
+    }
+
+    public static final Creator<Book> CREATOR = new Creator<Book>() {
+        @Override
+        public Book createFromParcel(Parcel in) {
+            return new Book(in);
+        }
+
+        @Override
+        public Book[] newArray(int size) {
+            return new Book[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel out, int flags) {
+        out.writeLong(id);
+        out.writeString(name);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
