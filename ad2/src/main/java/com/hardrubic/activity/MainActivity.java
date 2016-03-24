c41a4224223584235c4e86292cfbe27852ab5382
dgzx106 <dgzx106@163.com>
2016-03-24 15:03:00 +0800

    引入ButterKnife

--- ad2/src/main/java/com/hardrubic/activity/MainActivity.java
+++ ad2/src/main/java/com/hardrubic/activity/MainActivity.java
@@ -1,83 +1,105 @@
 package com.hardrubic.activity;
 
+import ad2.hardrubic.com.androiddemo20.R;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
-import ad2.hardrubic.com.androiddemo20.R;
+import android.widget.TextView;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends TitleActivity implements View.OnClickListener {
+
+    @Bind(R.id.tv_three_scroll)
+    TextView tv_three_scroll;
+    @Bind(R.id.tv_horizontal_scroll)
+    TextView tv_horizontal_scroll;
+    @Bind(R.id.tv_aidl)
+    TextView tv_aidl;
+    @Bind(R.id.tv_recycle_view)
+    TextView tv_recycle_view;
+    @Bind(R.id.tv_custom_view)
+    TextView tv_custom_view;
+    @Bind(R.id.tv_custom_view_group)
+    TextView tv_custom_view_group;
+    @Bind(R.id.tv_listview_swipe)
+    TextView tv_listview_swipe;
+    @Bind(R.id.tv_ExpandableListView_swipe)
+    TextView tv_ExpandableListView_swipe;
+    @Bind(R.id.tv_js_bridge)
+    TextView tv_js_bridge;
+    @Bind(R.id.tv_ndk)
+    TextView tv_ndk;
+    @Bind(R.id.tv_hide_dialog)
+    TextView tv_hide_dialog;
+    @Bind(R.id.tv_list_view_edit)
+    TextView tv_list_view_edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         hideTitleBack();
+        ButterKnife.bind(this);
 
         initView();
     }
 
     private void initView() {
-        findViewById(R.id.tv_three_scroll).setOnClickListener(this);
-        findViewById(R.id.tv_horizontal_scroll).setOnClickListener(this);
-        findViewById(R.id.tv_aidl).setOnClickListener(this);
-        findViewById(R.id.tv_recycle_view).setOnClickListener(this);
-        findViewById(R.id.tv_custom_view).setOnClickListener(this);
-        findViewById(R.id.tv_custom_view_group).setOnClickListener(this);
-        findViewById(R.id.tv_listview_swipe).setOnClickListener(this);
-        findViewById(R.id.tv_ExpandableListView_swipe).setOnClickListener(this);
-        findViewById(R.id.tv_js_bridge).setOnClickListener(this);
-        findViewById(R.id.tv_ndk).setOnClickListener(this);
-        findViewById(R.id.tv_hide_dialog).setOnClickListener(this);
+        tv_three_scroll.setOnClickListener(this);
+        tv_horizontal_scroll.setOnClickListener(this);
+        tv_aidl.setOnClickListener(this);
+        tv_recycle_view.setOnClickListener(this);
+        tv_custom_view.setOnClickListener(this);
+        tv_custom_view_group.setOnClickListener(this);
+        tv_listview_swipe.setOnClickListener(this);
+        tv_ExpandableListView_swipe.setOnClickListener(this);
+        tv_js_bridge.setOnClickListener(this);
+        tv_ndk.setOnClickListener(this);
+        tv_hide_dialog.setOnClickListener(this);
+        tv_list_view_edit.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+        Intent intent = null;
         switch (v.getId()) {
             case R.id.tv_three_scroll:
-                Intent intent1 = new Intent(this, ScrollActivity.class);
-                startActivity(intent1);
+                intent = new Intent(this, ScrollActivity.class);
                 break;
             case R.id.tv_horizontal_scroll:
-                Intent intent2 = new Intent(this, HorizontalScrollActivity.class);
-                startActivity(intent2);
+                intent = new Intent(this, HorizontalScrollActivity.class);
                 break;
             case R.id.tv_aidl:
-                Intent intent3 = new Intent(this, AidlActivity.class);
-                startActivity(intent3);
+                intent = new Intent(this, AidlActivity.class);
                 break;
             case R.id.tv_recycle_view:
-                Intent intent4 = new Intent(this, RecyclerViewActivity.class);
-                startActivity(intent4);
+                intent = new Intent(this, RecyclerViewActivity.class);
                 break;
             case R.id.tv_custom_view:
-                Intent intent6 = new Intent(this, CustomViewActivity.class);
-                startActivity(intent6);
+                intent = new Intent(this, CustomViewActivity.class);
                 break;
             case R.id.tv_custom_view_group:
-                Intent intent7 = new Intent(this, CustomViewGroupActivity.class);
-                startActivity(intent7);
+                intent = new Intent(this, CustomViewGroupActivity.class);
                 break;
             case R.id.tv_listview_swipe:
-                Intent intent8 = new Intent(this, ListViewSwipeActivity.class);
-                startActivity(intent8);
+                intent = new Intent(this, ListViewSwipeActivity.class);
                 break;
             case R.id.tv_ExpandableListView_swipe:
-                Intent intent9 = new Intent(this, ExpandableListViewSwipeActivity.class);
-                startActivity(intent9);
+                intent = new Intent(this, ExpandableListViewSwipeActivity.class);
                 break;
             case R.id.tv_js_bridge:
-                Intent intent10 = new Intent(this, JsTestActivity.class);
-                startActivity(intent10);
+                intent = new Intent(this, JsTestActivity.class);
                 break;
             case R.id.tv_ndk:
-                Intent intent11 = new Intent(this, NdkActivity.class);
-                startActivity(intent11);
+                intent = new Intent(this, NdkActivity.class);
                 break;
             case R.id.tv_hide_dialog:
-                Intent intent12 = new Intent(this, HideDialogActivity.class);
-                startActivity(intent12);
+                intent = new Intent(this, HideDialogActivity.class);
+                break;
+            default:
                 break;
         }
+        startActivity(intent);
     }
 }
