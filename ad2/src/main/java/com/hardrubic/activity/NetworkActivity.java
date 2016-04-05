a82f693b8a121ac5131b1f82559bcfbcc5d49cf6
hardrubic <dgzx106@163.com>
2016-04-06 07:13:16 +0800

    启用lambda

--- ad2/src/main/java/com/hardrubic/activity/NetworkActivity.java
+++ ad2/src/main/java/com/hardrubic/activity/NetworkActivity.java
@@ -1,11 +1,14 @@
 package com.hardrubic.activity;
 
 import ad2.hardrubic.com.androiddemo20.R;
+
 import android.content.Context;
 import android.os.Bundle;
 import android.text.TextUtils;
+
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+
 import com.hardrubic.Constants;
 import com.hardrubic.entity.db.Project;
 import com.hardrubic.entity.db.Team;
@@ -17,22 +20,29 @@ import com.hardrubic.util.network.HttpException;
 import com.hardrubic.util.network.HttpManager;
 import com.hardrubic.util.network.HttpService;
 import com.hardrubic.util.network.SyncExecutorServiceUtil;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import rx.Observable;
 import rx.Subscriber;
-import rx.functions.Action1;
 import rx.schedulers.Schedulers;
 
 public class NetworkActivity extends TitleActivity {
     private Context mContext;
     private String token;
 
-    /** 同步项目接口 */
+    /**
+     * 同步项目接口
+     */
     public static final String ERROR_CODE_E001 = "E001";
-    /** 同步用户与角色接口 */
+    /**
+     * 同步用户与角色接口
+     */
     public static final String ERROR_CODE_E002 = "E002";
-    /** 同步项目检查项接口 */
+    /**
+     * 同步项目检查项接口
+     */
     public static final String ERROR_CODE_E003 = "E003";
 
 
@@ -90,46 +100,37 @@ public class NetworkActivity extends TitleActivity {
      * project列表
      */
     private void pullProjectList() {
-        Observable.create(new Observable.OnSubscribe<String>() {
-            @Override
-            public void call(Subscriber<? super String> subscriber) {
-                LogUtils.d("project列表线程:" + Thread.currentThread().getId());
+        Observable.create((Subscriber<? super String> subscriber) -> {
+            LogUtils.d("project列表线程:" + Thread.currentThread().getId());
 
-                List<Long> idList = new ArrayList<>();
-                try {
-                    ProjectListResponse response = HttpService.applyProjectList(token);
-                    if (response.getResult() != Constants.RESPOND_RESULT_OK) {
-                        throw new HttpException(ERROR_CODE_E001);
-                    }
-
-                    List<Project> projectList = response.getData().getProjects();
-                    List<Team> teamList = response.getData().getTeams();
-                    int projectSize = projectList == null ? 0 : projectList.size();
-                    int teamSize = teamList == null ? 0 : teamList.size();
-                    LogUtils.d("同步项目成功，数量：projectList_" + projectSize + " teamList_" + teamSize);
-
-                    for (Project project : projectList) {
-                        idList.add(project.getId());
-                    }
-                } catch (HttpException e) {
-                    subscriber.onError(e);
+            List<Long> idList = new ArrayList<>();
+            try {
+                ProjectListResponse response = HttpService.applyProjectList(token);
+                if (response.getResult() != Constants.RESPOND_RESULT_OK) {
+                    throw new HttpException(ERROR_CODE_E001);
                 }
 
-                subscriber.onNext(TextUtils.join(",", idList));
+                List<Project> projectList = response.getData().getProjects();
+                List<Team> teamList = response.getData().getTeams();
+                int projectSize = projectList == null ? 0 : projectList.size();
+                int teamSize = teamList == null ? 0 : teamList.size();
+                LogUtils.d("同步项目成功，数量：projectList_" + projectSize + " teamList_" + teamSize);
+
+                for (Project project : projectList) {
+                    idList.add(project.getId());
+                }
+            } catch (HttpException e) {
+                subscriber.onError(e);
             }
+
+            subscriber.onNext(TextUtils.join(",", idList));
         })
                 .subscribeOn(Schedulers.from(SyncExecutorServiceUtil.getFixedThreadPool()))
                 .observeOn(Schedulers.newThread())
-                .subscribe(new Action1() {
-                    @Override
-                    public void call(Object o) {
-                        LogUtils.d("onNext");
-                    }
-                }, new Action1<Throwable>() {
-                    @Override
-                    public void call(Throwable throwable) {
-                        throwable.printStackTrace();
-                    }
+                .subscribe(o -> {
+                    LogUtils.d("onNext");
+                }, throwable -> {
+                    throwable.printStackTrace();
                 });
     }
 
