b0ea320d2f7f13cbf641d964bf8bf315685c6a71
yiheng <yiheng@ziipin.com>
2016-09-23 08:41:56 +0800

    线程中断测试

---
+++ concurrent_example/src/main/java/com/hardrubic/concurrent/InterruptedExample.java
@@ -0,0 +1,59 @@
+package com.hardrubic.concurrent;
+
+/**
+ * 线程中断例子
+ */
+public class InterruptedExample {
+
+    /**
+     * 1、调用interrupt并不意味着立即停止目标线程，只是传递了请求中断；
+     * 2、只有实现了线程中断策略的代码才可以屏蔽中断，否则要保持中断状态
+     */
+
+    public static void main(String[] args) throws Exception {
+        InterruptedExample interruptedExample = new InterruptedExample();
+        interruptedExample.start();
+    }
+
+    public void start() {
+        MyThread myThread = new MyThread();
+        myThread.start();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        myThread.cancel();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class MyThread extends Thread{
+
+        @Override
+        public void run() {
+            while (!Thread.currentThread().isInterrupted()) {   //判断线程是否被中断
+                try {
+                    System.out.println("test");
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.out.println("interrupt");
+                    //如果不触发InterruptedException，中断会保持
+                    //抛出InterruptedException后中断标志被清除，标准做法是再次调用interrupt恢复中断
+                    Thread.currentThread().interrupt();
+                }
+            }
+            System.out.println("stop");
+        }
+
+        public void cancel(){
+            interrupt();
+        }
+    }
+}
